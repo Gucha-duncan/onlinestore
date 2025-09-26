75180311db14e7296689af0eeb4873e7f42b2dbd
Gucha_duncan <gucha_duncan@outlook.com>
2025-09-26 17:46:10 +0300

    Migrations of the models into the database

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Promotion)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(CartItem)
+admin.site.register(Cart)
+admin.site.register(Collection)
+admin.site.register(Customer) 
--- store/models.py
+++ store/models.py
@@ -1,13 +1,26 @@
 from django.db import models
-from django.contrib.auth import User
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Promotion(models.Model):
+    discount = models.FloatField()
+    description = models.CharField(max_length=255)
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    
 class Product(models.Model):
     title = models.CharField(max_length= 255)
+    slug = models.SlugField()
     description =models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
+
+
+    title = models.CharField(max_length=255)
     
 class Customer(models.Model):
     MEMBERSHIP_CHOICES =[
@@ -25,7 +38,7 @@ class Customer(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    customer = models.OneToOneField(Customer, on_delete=models.CASCADE)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
     
 class Order(models.Model):
     PAYMENT_STATUS = [
@@ -35,3 +48,19 @@ class Order(models.Model):
     ]
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(choices=PAYMENT_STATUS)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete= models.PROTECT)
+    product = models.ForeignKey(Product,  on_delete= models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits= 6, decimal_places= 2)
+    
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add= True)
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete= models.CASCADE)
+    product = models.ForeignKey(Product, on_delete= models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
